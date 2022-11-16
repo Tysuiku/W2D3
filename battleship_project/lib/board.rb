321138cd504173cd28efc46c680367915b28c5e9
Tysuiku <Timothydong41@gmail.com>
2022-11-16 16:33:13 -0500

    done with board

--- battleship_project/lib/board.rb
+++ battleship_project/lib/board.rb
@@ -1,6 +1,12 @@
 class Board
     attr_reader :size
 
+    def self.print_grid(grid)
+        grid.each do |row|
+          puts row.join(" ")
+    end
+end
+
     def initialize(n)
         @grid = Array.new(n) { Array.new(n, :N) }
         @size = n * n
@@ -14,4 +20,52 @@ class Board
     def []=(position, value)
         row, column = position
         @grid [row][column] = value
+    end
+
+    def num_ships
+        @grid.flatten.count { |ele| ele == :S }
+    end
+
+    def attack(position)
+        if self[position] == :S
+            self[position] = :H
+            puts "you sunk my battleship!"
+            return true
+        else
+            self[position] = :X
+            return false
+        end
+    end
+
+    def place_random_ships
+        total_ships = @size * 0.25
+
+        while self.num_ships < total_ships
+        random_row = rand(0...@grid.length) 
+        random_column = rand(0...@grid.length) 
+        pos = [random_row, random_column]
+        self[pos] = :S
+        end
+    end
+
+    def hidden_ships_grid
+        @grid.map do |row|
+            row.map do |ele|
+                if ele == :S
+                    :N
+                else
+                    ele
+                end
+            end
+         end
+    end
+
+    def cheat
+        Board.print_grid(@grid)
+    end
+
+    def print
+        Board.print_grid(self.hidden_ships_grid)
+    end
 end
+
