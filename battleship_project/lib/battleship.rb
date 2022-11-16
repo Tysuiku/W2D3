768144921146daa61bef6ed482f83bfe04bc6da7
Tysuiku <Timothydong41@gmail.com>
2022-11-16 18:17:51 -0500

    worked on battleShip

--- battleship_project/lib/battleship.rb
+++ battleship_project/lib/battleship.rb
@@ -2,5 +2,17 @@ require_relative "board"
 require_relative "player"
 
 class Battleship
+    attr_reader :board, :player
 
+    def initialize(n)
+        @player = Player.new
+        @board = Board.new(n)
+        @remaining_misses = @board.size * 0.5
+    end
+
+    def start_game
+        @board.place_random_ships
+        puts @board.num_ships
+        @board.print
+    end
 end
