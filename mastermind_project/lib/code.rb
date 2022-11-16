92eb10538bc79fb28e750a9c99825da535516310
Tysuiku <Timothydong41@gmail.com>
2022-11-16 18:59:37 -0500

    working on mastermind

--- mastermind_project/lib/code.rb
+++ mastermind_project/lib/code.rb
@@ -5,5 +5,18 @@ class Code
     "B" => :blue,
     "Y" => :yellow
   }
+  
+  def self.valid_pegs?(chars)
+    chars.all? { |char| POSSIBLE_PEGS[char.upcase] != nil}
+  end
 
+  def initialize(chars)
+    if Code.valid_pegs?(chars)
+    @pegs = chars.map { |char| char.upcase }
+    else
+      raise "pegs are not valid"
+    end
+  end
+
+  
 end
