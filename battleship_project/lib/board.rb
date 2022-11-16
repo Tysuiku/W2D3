709d79af7058a5ad354ce94b12c3a36309d8da7e
Tysuiku <Timothydong41@gmail.com>
2022-11-16 15:31:41 -0500

    working on the board

--- battleship_project/lib/board.rb
+++ battleship_project/lib/board.rb
@@ -1,3 +1,17 @@
 class Board
-  
+    attr_reader :size
+
+    def initialize(n)
+        @grid = Array.new(n) { Array.new(n, :N) }
+        @size = n * n
+    end
+
+    def [](position)
+        row, column = position
+        @grid [row][column]
+    end
+
+    def []=(position, value)
+        row, column = position
+        @grid [row][column] = value
 end
